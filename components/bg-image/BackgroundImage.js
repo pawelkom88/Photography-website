f6505d3d9c5c0facd6f0560da3dd00a1f2936831
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-10-27 19:12:15 +0100

    fix: Fix typo

---
+++ components/bg-image/BackgroundImage.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import styles from "../../layout/overlay/overlay.module.css";
+
+export default function BackgroundImage({ onHover, position }) {
+  useEffect(() => {
+    const bgImages = Array.from(document.querySelectorAll(".bg-image"));
+
+    bgImages.forEach((img, i) => {
+      if (onHover && position === i) {
+        img.classList.add(`${styles.on}`);
+        img.classList.remove(`${styles.off}`);
+      } else {
+        img.classList.add(`${styles.off}`);
+        img.classList.remove(`${styles.on}`);
+      }
+    });
+  }, [onHover, position]);
+
+  // let imageOverlay = onHover ? styles.on : styles.off;
+
+  return (
+    <>
+      <div className={`bg-image ${styles["bg-image1"]} overlay`}></div>
+      <div className={`bg-image ${styles["bg-image2"]} overlay`}></div>
+      <div className={`bg-image ${styles["bg-image3"]} overlay`}></div>
+      <div className={`bg-image ${styles["bg-image4"]} overlay`}></div>
+      <div className={`bg-image ${styles["bg-image5"]} overlay`}></div>
+      <div className={`bg-image ${styles["bg-image6"]} overlay`}></div>
+    </>
+  );
+}
