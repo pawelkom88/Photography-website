7165be2e5ab9a151e9e463b3e28332896751104f
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-10-31 16:22:14 +0000

    refactor: extract section with video background

--- pages/weddings.js
+++ pages/weddings.js
@@ -2,33 +2,16 @@
 import "@fontsource/questrial";
 import "@fontsource/marcellus-sc";
 
-import { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
+import SectionHero from "../components/section/SectionHero";
 import Logo from "../components/logo/Logo";
 import Header from "../layout/header/Header";
 import Button from "../components/button/Button";
 import Hamburger from "../components/hamburger/Hamburger";
-import Overlay from "../layout/overlay/Overlay";
-import { motion } from "framer-motion";
 
 // styles
-import styles from "../styles/section.module.css";
 
 export default function Weddings({ mediaQueries }) {
-  const [isPlaying, setIsPlaying] = useState(true);
-
-  const variants = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: {
-        ease: "easeIn",
-        duration: 1,
-      },
-    },
-  };
-
   return (
     <>
       <div className="container">
@@ -47,32 +30,11 @@ export default function Weddings({ mediaQueries }) {
           </div>
         </Header>
       </div>
-      {/* extract component */}
-      <>
-        <section className={styles.section}>
-          {!isPlaying ? (
-            <Overlay>
-              <motion.div key="image" initial="initial" animate="animate" variants={variants}>
-                <Image layout="fill" objectFit="cover" src="/assets/images/w1.webp" alt="wedding" />
-              </motion.div>
-            </Overlay>
-          ) : (
-            <>
-              <video
-                onEnded={() => setIsPlaying(false)}
-                className={styles.video}
-                muted={true}
-                preload="auto"
-                autoPlay="autoplay"
-                type="video/mp4">
-                <source src="/assets/videos/wedding_video.mp4" />
-              </video>
-              <p>Scroll down</p>
-            </>
-          )}
-          <h2 className={styles["section-heading"]}>Weddings</h2>
-        </section>
-      </>
+      <SectionHero
+        section="Weddings"
+        imageSrc="/assets/images/w1.webp"
+        videoSrc="/assets/videos/wedding_video.mp4"
+      />
       <section className="container">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum rerum consequatur
         quibusdam quod molestiae quae deleniti voluptatibus, reiciendis obcaecati quaerat dicta iure
