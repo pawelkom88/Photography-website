f5f8df67dec9432ea77964b41a239c54bf1eac59
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-10-31 14:19:46 +0000

    refactor: move useMedia hook to app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,36 @@
+// fonts
+import "@fontsource/questrial";
+import "@fontsource/marcellus-sc";
+
+import useMatchMedia from "../hooks/useMatchMedia";
 import { motion } from "framer-motion";
+import { screenSize } from "../helpers/helpers";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps, router }) {
+  const { matches } = useMatchMedia(screenSize);
+
   const variants = {
     initial: {
       opacity: 0,
     },
     animate: {
       opacity: 1,
+      transition: { duration: 1 },
     },
     exit: { opacity: 0 },
-    transition: { ease: "easeInOut", duration: 3 },
+    transition: { ease: "easeInOut", duration: 1 },
   };
 
   return (
     <>
-      <motion.div key={router.route} initial="initial" animate="animate" variants={variants}>
-        <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={variants}>
+        <Component mediaQueries={matches} {...pageProps} />
       </motion.div>
     </>
   );
