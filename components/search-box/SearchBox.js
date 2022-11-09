fd5da5f4685dcf1435c3fc5136c157e071dca345
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-09 20:26:32 +0000

    chore: Add new classes and styles

--- components/search-box/SearchBox.js
+++ components/search-box/SearchBox.js
@@ -5,19 +5,28 @@ import styles from "./search-box.module.css";
 export default function SearchBox({ setCategory }) {
   const [userInput, setUserInput] = useState("");
 
-  function handleCategory() {
+  function handleCategory(e) {
     setUserInput(e.target.value);
   }
 
-  function handleSubmit() {
+  function handleSubmit(e, input) {
     e.preventDefault();
-    setCategory();
+    setCategory(input);
     setUserInput("");
   }
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <label htmlFor="category">Category</label>
-      <input id="category" type="text" value={userInput} onChange={handleCategory} />
+    <form onSubmit={e => handleSubmit(e, userInput)} className={styles.form}>
+      <label htmlFor="category">
+        <input
+          className={styles.input}
+          id="category"
+          type="text"
+          value={userInput}
+          onChange={handleCategory}
+          placeholder="Enter category name"
+          required
+        />
+      </label>
       <Button>Submit</Button>
     </form>
   );
