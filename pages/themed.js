9a78e54b74c06d3a6e66f480f93a36f7e071b9ad
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-12 12:48:01 +0000

    feat: Add themed page

---
+++ pages/themed.js
@@ -0,0 +1,51 @@
+import SectionHero from "@components/section/SectionHero";
+import SectionDescribtion from "@components/section-description/SectionDescribtion";
+import SectionImages from "@components/section-images/SectionImages";
+import Logo from "@components/logo/Logo";
+import Header from "@layout/header/Header";
+import Hamburger from "@components/hamburger/Hamburger";
+import Marquee from "react-fast-marquee";
+
+// styles
+import styles from "@styles/marquee.module.css";
+
+// helpers
+import { sectionData } from "../helpers/helpers";
+
+export default function Themed({ isOpen, setIsOpen }) {
+  return (
+    <>
+      <div className="container">
+        <Header>
+          <Logo></Logo>
+          <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
+        </Header>
+      </div>
+      {sectionData.map(({ id, linkName, link, video, photosDescription }) => {
+        if (linkName === "Themed") {
+          return (
+            <div key={id}>
+              <SectionHero section={linkName} imageSrc={link.slice(4, -1)} videoSrc={video} />
+              <SectionDescribtion title={linkName} />
+              <SectionImages
+                heading={linkName}
+                photosDescription={photosDescription}
+                category={linkName}
+              />
+              <Marquee style={{ marginTop: "7rem" }} className={styles.marquee} speed={60}>
+                {linkName}
+              </Marquee>
+              <Marquee
+                style={{ marginBottom: "6rem" }}
+                className={styles.marquee}
+                direction="right"
+                speed={60}>
+                {linkName}
+              </Marquee>
+            </div>
+          );
+        }
+      })}
+    </>
+  );
+}
